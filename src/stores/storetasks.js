c06c4e5c502c560c3fcb0a4a267fa8826bcd44f0
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-09 18:48:09 +0200

    Store Functions added

--- src/stores/storetasks.js
+++ src/stores/storetasks.js
@@ -4,5 +4,12 @@ import { defineStore } from 'pinia'
 
 export const useStoreTasks = defineStore('storetasks', () => {
 
+  const tasks = ref([])
 
+  const status = ref('Ongoing')
+
+  const totalTasks = computed(() => tasks.value.length)
+
+  return { tasks, totalTasks, status }
+  
 })
