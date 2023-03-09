d1ec24ef41a5ca255bbbd77c35755e7ad673f4b5
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-09 18:50:28 +0200

    Functions in App added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,26 @@
     import { useStoreTasks } from '@/stores/storetasks'
     import axios from 'axios'
 
+    const store = useStoreTasks()
 
+    const {  tasks, totalTasks, status } = storeToRefs(store)
+    
+    const taks_input = ref('')
+
+    const addtask = async () => {
+        alert(totalTasks.value)
+        const v = { "id": (Number(totalTasks) + 1), "task":taks_input.value, "status":"Ongoing", "favorite":0 }
+        const res = await axios.post('http://127.0.0.1:3000/task/', v,
+          {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+          }
+        );
+        console.log(res);
+    }
+    
 </script>
 
 <style>
